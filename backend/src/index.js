af447252e56f535b3478640cdacd1cf7e4eb4fe3
prativzz <166499719+prativzz@users.noreply.github.com>
2025-08-07 02:48:21 +0530

    some message

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,10 +31,11 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("/*", (req, res) => {
+  app.all("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
+
 
 
 server.listen(PORT, ()=>{
