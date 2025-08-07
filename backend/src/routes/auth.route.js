e2f1c7e3fc6df698501925abcabe5bf3464b6737
prativzz <166499719+prativzz@users.noreply.github.com>
2025-08-07 21:40:49 +0530

    new

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,12 @@
 import express from "express";
 import {signup,login,logout,updateprofile,checkAuth} from "../controllers/auth.controller.js"
 import {protectRoute} from "../middleware/auth.middleware.js"
- const router = express.Router();
- router.post("/signup", signup)
-  router.post("/login", login)
-  router.post("/logout", logout)
- 
-  router.put("/update-profile",protectRoute,updateprofile)
- 
-    router.get("/check",protectRoute,checkAuth)
+const router = express.Router();
 
-  export default router;
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.put("/update-profile", protectRoute, updateprofile);
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {getUsersforSidebar,getMessages,sendMessages,deleteMessage} from "../controllers/message.controller.js"
 import {protectRoute} from "../middleware/auth.middleware.js"
- const router = express.Router();
- 
- router.get("/users",protectRoute,getUsersforSidebar)
-  router.get("/:id",protectRoute,getMessages)
- router.post("/send/:id",protectRoute,sendMessages)
-router.delete('/:messageId', protectRoute, deleteMessage); 
+const router = express.Router();
 
-  export default router;
+router.get("/users", protectRoute, getUsersforSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessages);
+router.delete("/:messageId", protectRoute, deleteMessage);
+
+export default router;
