eb45aa1f8d1a5feda6819d39f8b1979e6ce50228
prativzz <166499719+prativzz@users.noreply.github.com>
2025-08-07 21:56:44 +0530

    fixed cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://fullstack-chat-app-idtb.vercel.app',
   credentials: true,
 }));
 
